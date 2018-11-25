715e8fec64d7012d38d185937c21e3de3205e766
Sruthi Krishna <sruthik926@gmail.com>
2018-11-25 19:36:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  let newStr = "";
+  let newStr = "" + "'";
   let finArr = [];
   for(let i = 0; i < musicians.length; i++){
-      newStr += musicians[i] + " plays " + "" + instruments[i] + ",'" + "";
+      newStr += musicians[i] + " plays " + "" + instruments[i] + "," + "";
       
   }
     finArr.push(newStr);
