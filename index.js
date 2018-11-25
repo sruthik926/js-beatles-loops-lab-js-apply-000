fa7ac65cd2703e5dec345f65b01141cefbfee7bd
Sruthi Krishna <sruthik926@gmail.com>
2018-11-25 19:53:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ function theBeatlesPlay(musicians, instruments){
 function johnLennonFacts(facts){
   let exclamPoint = [];
   i = 0;
-  while(i < facts.length)
+  while(i < facts.length){
     exclamPoint.push(facts[i] + "!!!");
      i += 1;
+  }
   return exclamPoint;
 }
  
