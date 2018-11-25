16f63236f02cbf570467e583dd4a422dff35c675
Sruthi Krishna <sruthik926@gmail.com>
2018-11-25 19:45:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments){
   let newStr = "";
   let musiciansGroup = [];
   for(let i = 0; i < musicians.length; i++){
-       musiciansGroup.push(musicians[i] + "plays" + instruments[i])
+       musiciansGroup.push(musicians[i] + "plays" + instruments[i]);
       
   }
     
